8350cccab65b0a5a311bc9f5506d7ebe1e2c0895
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2025-04-09 21:02:17 -0400

    Setlist basic functionality

--- seekdeeplearning/src/app/page.tsx
+++ seekdeeplearning/src/app/page.tsx
@@ -13,6 +13,10 @@ import { useRouter } from 'next/navigation'; // Import useRouter from next/navig
       router.push('/AlePage'); // Navigate to /ale page
    };
 
+   const navigateToSetList = () => {
+    router.push('/SetList'); // Navigate to /ale page
+ };
+
 
    return (
     <div className="p-6 space-y-6">
@@ -30,6 +34,12 @@ import { useRouter } from 'next/navigation'; // Import useRouter from next/navig
       onClick={navigateToALEPage} // On button click, navigate to flashcards page
       >
         Go to ALE Page
+      </button>
+      <button
+      className="bg-[#E0E4F5] text-black p-4 rounded-xl shadow"
+      onClick={navigateToSetList} // On button click, navigate to flashcards page
+      >
+        Go to Set List
       </button>
       </div>
    );
