4eea623a2ba49ba6b7f7ee9815767fc4b5025a83
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2025-04-11 11:25:15 -0400

    added more to the seekhoot page

---
+++ seekdeeplearning/src/app/components/seekhootjoin/hootusers.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './hootusers.module.css';
+
+const HootUser: React.FC<{}> = () => {
+    return (
+	<div className={styles.userlist}>
+					     hello
+	</div>
+    )
+}
+
+export default HootUser;
