dfc6cf3f0e7b8ccb4edcd5aaf05b2b235b500e79
Katie Cheng <katiecheng@Katies-MacBook-Pro.local>
2025-04-18 12:40:44 -0400

    small change to the search bar

--- seekdeeplearning/src/app/page.tsx
+++ seekdeeplearning/src/app/page.tsx
@@ -41,7 +41,7 @@ const HomePage = () => {
               <Search className="absolute left-3 top-2.5 text-gray-500 w-5 h-5" />
               <input
                 type="text"
-                placeholder="Search your SetList titles..."
+                placeholder="Search for flashcard sets..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
